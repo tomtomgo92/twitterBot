91922f996da3b422eb738a96495cd099cde0290a
Thomas Millet <tm.lepoleswebforce3@gmail.com>
2018-07-27 11:05:23 +0200

    maj

---
+++ twitter-bot/app.js
@@ -0,0 +1,45 @@
+// Dependencies =========================
+var
+    twit = require('twit'),
+    config = require('./config');
+
+var Twitter = new twit(config);
+
+// RETWEET BOT ==========================
+
+// find latest tweet according the query 'q' in params
+var retweet = function() {
+    var params = {
+        q: 'recherche une alternance',  // REQUIRED
+        result_type: 'recent',
+        lang: 'fr'
+    }
+    Twitter.get('search/tweets', params, function(err, data) {
+      // if there no errors
+        if (!err) {
+          // grab ID of tweet to retweet
+            var retweetId = data.statuses[0].id_str;
+            // Tell TWITTER to retweet
+            Twitter.post('statuses/retweet/:id', {
+                id: retweetId
+            }, function(err, response) {
+                if (response) {
+                    console.log('Retweeted!!!');
+                }
+                // if there was an error while tweeting
+                if (err) {
+                    console.log('Something went wrong while RETWEETING... Duplication maybe...');
+                }
+            });
+        }
+        // if unable to Search a tweet
+        else {
+          console.log('Something went wrong while SEARCHING...');
+        }
+    });
+}
+
+// grab & retweet as soon as program is running...
+retweet();
+// retweet in every 50 minutes
+setInterval(retweet, 3000000);
---
+++ twitter-bot/config.js
@@ -0,0 +1,13 @@
+/config.js
+/** TWITTER APP CONFIGURATION
+ * consumer_key
+ * consumer_secret
+ * access_token
+ * access_token_secret
+ */
+module.exports = {
+  consumer_key: 'Ip4uxqAEsuQW6V2oo9zixCA1a',  
+  consumer_secret: 'K98QrnOrDNbxe9fvTOitqNEQNDbguNLhMlYcQ4aklwwmE2HoAH',
+  access_token: '998923426025918465-TQNeKnk9fgoHrzDiQbjut598vNSMEAS',  
+  access_token_secret: 'MJuWfQu4lmZqi7vQDjrF5Bks3DQZvQ7WUbKHjloUPRkRA'
+}
