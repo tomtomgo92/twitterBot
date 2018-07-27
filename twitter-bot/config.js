a543a9584e1c93ed22068917316c77079dd72641
Thomas Millet <tm.lepoleswebforce3@gmail.com>
2018-07-27 11:14:03 +0200

    sans token c'est mieux :)

--- twitter-bot/config.js
+++ twitter-bot/config.js
@@ -6,8 +6,8 @@
  * access_token_secret
  */
 module.exports = {
-  consumer_key: 'Ip4uxqAEsuQW6V2oo9zixCA1a',  
-  consumer_secret: 'K98QrnOrDNbxe9fvTOitqNEQNDbguNLhMlYcQ4aklwwmE2HoAH',
-  access_token: '998923426025918465-TQNeKnk9fgoHrzDiQbjut598vNSMEAS',  
-  access_token_secret: 'MJuWfQu4lmZqi7vQDjrF5Bks3DQZvQ7WUbKHjloUPRkRA'
+  consumer_key: '*',  
+  consumer_secret: '**',
+  access_token: '****',  
+  access_token_secret: '****'
 }
